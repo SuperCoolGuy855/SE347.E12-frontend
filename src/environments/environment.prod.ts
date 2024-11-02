c09c889a38ee3238af3ceaaf5d3f666523093006
Mai Phuc Minh <minhminh99599@gmail.com>
2024-11-02 09:28:38 +0700

    Change localhost binding to all IPs binding

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import { apiPath } from './../app/share/constance/api-path';
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5000',
+  apiUrl: 'http://0.0.0.0:5000',
   // apiUrl: 'https://api.hieud.me',
-  url: 'http://localhost:4200'
+  url: 'http://0.0.0.0:4200'
 };
